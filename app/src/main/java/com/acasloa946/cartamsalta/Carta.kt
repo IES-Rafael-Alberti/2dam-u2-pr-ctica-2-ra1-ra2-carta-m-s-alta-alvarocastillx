1629c3d47b6059fa631ace157f97ff3bbb5f2b32
Álvaro C <castillaloaizaalvaro@gmail.com>
2023-11-14 20:40:54 +0100

    99% (Faltan comentarios)

--- app/src/main/java/com/acasloa946/cartamsalta/Carta.kt
+++ app/src/main/java/com/acasloa946/cartamsalta/Carta.kt
@@ -2,4 +2,7 @@ package com.acasloa946.cartamsalta
 
 class Carta(nombre:Naipes,palo:Palos,puntosMin:Int,puntosMax:Int,idDrawable:Int) {
     var IdDrawable = idDrawable
+    var Nombre = nombre
+    var Palo = palo
+    constructor():this(Naipes.AS,Palos.PICAS,0,0,0)
 }
--- app/src/main/java/com/acasloa946/cartamsalta/Screens/UIControl.kt
+++ app/src/main/java/com/acasloa946/cartamsalta/Screens/UIControl.kt
@@ -30,72 +30,77 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.acasloa946.cartamsalta.Baraja
+import com.acasloa946.cartamsalta.Carta
 import com.acasloa946.cartamsalta.R
 
+var cartaNueva : Carta = Carta()
 class UIControl {
 
-    val listaDeResources = mutableListOf<Int>(
+    val listaDeResources = mutableListOf(
         R.drawable.as1,
         R.drawable.as2,
         R.drawable.as3,
         R.drawable.as4,
         R.drawable.c2,
-        R.drawable.c3,
-        R.drawable.c4,
-        R.drawable.c5,
-        R.drawable.c6,
-        R.drawable.c7,
-        R.drawable.c8,
-        R.drawable.c9,
-        R.drawable.c10,
-        R.drawable.c11,
-        R.drawable.c12,
-        R.drawable.c13,
         R.drawable.c15,
-        R.drawable.c16,
-        R.drawable.c17,
-        R.drawable.c18,
-        R.drawable.c19,
-        R.drawable.c20,
-        R.drawable.c21,
-        R.drawable.c22,
-        R.drawable.c23,
-        R.drawable.c24,
-        R.drawable.c25,
-        R.drawable.c26,
         R.drawable.c28,
-        R.drawable.c29,
-        R.drawable.c30,
-        R.drawable.c31,
-        R.drawable.c32,
-        R.drawable.c33,
-        R.drawable.c34,
-        R.drawable.c35,
-        R.drawable.c36,
-        R.drawable.c37,
-        R.drawable.c38,
-        R.drawable.c39,
         R.drawable.c41,
+        R.drawable.c3,
+        R.drawable.c16,
+        R.drawable.c29,
         R.drawable.c42,
+        R.drawable.c4,
+        R.drawable.c17,
+        R.drawable.c30,
         R.drawable.c43,
+        R.drawable.c5,
+        R.drawable.c18,
+        R.drawable.c31,
         R.drawable.c44,
+        R.drawable.c6,
+        R.drawable.c19,
+        R.drawable.c32,
         R.drawable.c45,
+        R.drawable.c7,
+        R.drawable.c20,
+        R.drawable.c33,
         R.drawable.c46,
+        R.drawable.c8,
+        R.drawable.c21,
+        R.drawable.c34,
         R.drawable.c47,
+        R.drawable.c9,
+        R.drawable.c22,
+        R.drawable.c35,
         R.drawable.c48,
+        R.drawable.c10,
+        R.drawable.c23,
+        R.drawable.c36,
         R.drawable.c49,
+        R.drawable.c11,
+        R.drawable.c24,
+        R.drawable.c37,
         R.drawable.c50,
+        R.drawable.c12,
+        R.drawable.c25,
+        R.drawable.c38,
         R.drawable.c51,
+        R.drawable.c13,
+        R.drawable.c26,
+        R.drawable.c39,
         R.drawable.c52,
 
 
         )
 
 
+
     @Composable
     fun Inicio() {
         val context = LocalContext.current
         var imagenID by rememberSaveable { mutableStateOf(R.drawable.facedown) }
+
+
 
         Column(
             Modifier
@@ -107,10 +112,10 @@ class UIControl {
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            if (imagenID!=R.drawable.facedown) {
+            if (imagenID != R.drawable.facedown) {
                 ImagenBaraja(Baraja.listaCartas.size)
             }
-            CrearImagen(imagenID)
+            CrearImagen(imagenID,cartaNueva)
             Row(
                 modifier = Modifier.padding(top = 30.dp)
             ) {
@@ -118,14 +123,16 @@ class UIControl {
                     onClickDameCarta = {
                         if (imagenID == R.drawable.facedown) {
                             imagenID = Baraja.listaCartas.last().IdDrawable
-                        } else if (Baraja.ultimaCarta) {
+                        }
+                        else if (Baraja.ultimaCarta) {
                             Baraja.ultimaCarta = false
-                            Toast.makeText(context, "Última carta", Toast.LENGTH_SHORT).show()
-                        } else {
-                            val cartaNueva = Baraja.dameCarta()
+                            Toast.makeText(context, "Esta es la última carta", Toast.LENGTH_SHORT)
+                                .show()
+                        }
+                        else {
+                            cartaNueva = Baraja.dameCarta()
                             imagenID = cartaNueva.IdDrawable
                         }
-
                     },
                     onClickReiniciar = {
                         Baraja.borrarBaraja()
@@ -140,10 +147,10 @@ class UIControl {
     }
 
     @Composable
-    fun ImagenBaraja(numb : Int) {
-        Box (
-        contentAlignment = Alignment.Center
-        ){
+    fun ImagenBaraja(numb: Int) {
+        Box(
+            contentAlignment = Alignment.Center
+        ) {
 
             Image(
                 painter = painterResource(id = R.drawable.facedown),
@@ -151,7 +158,8 @@ class UIControl {
                 modifier = Modifier
                     .padding(bottom = 50.dp)
             )
-            Text(text = "${numb}",
+            Text(
+                text = "$numb",
                 textAlign = TextAlign.Center,
                 color = Color.White,
                 fontSize = 30.sp,
@@ -159,56 +167,64 @@ class UIControl {
                 modifier = Modifier.background(Color.Black)
             )
         }
+
     }
 
     @Composable
-    fun CrearImagen(imagenID: Int) {
-        if (imagenID == R.drawable.as1) {
-            Image(
-                painter = painterResource(id = imagenID),
-                contentDescription = null,
+    fun CrearImagen(imagenID: Int, Carta:Carta) {
+        if (imagenID!=R.drawable.facedown) {
+            Text(
+                text = "${Carta.Nombre} de ${Carta.Palo}",
+                textAlign = TextAlign.Center,
+                color = Color.Black,
+                fontSize = 24.sp,
+                fontStyle = FontStyle.Normal,
                 modifier = Modifier
-                    .size(350.dp)
-            )
-        } else {
-            Image(
-                painter = painterResource(id = imagenID),
-                contentDescription = null,
-                modifier = Modifier
-                    .size(350.dp)
+                    .padding(bottom = 10.dp)
             )
         }
+        Image(
+            painter = painterResource(id = imagenID),
+            contentDescription = null,
+            modifier = Modifier
+                .size(350.dp)
+        )
     }
 
-    @Composable
-    fun Botones(
-        onClickDameCarta: () -> Unit,
-        onClickReiniciar: () -> Unit
+}
+@Composable
+
+fun Botones(
+    onClickDameCarta: () -> Unit,
+    onClickReiniciar: () -> Unit
+) {
+    Button(
+        onClick = {
+            if (Baraja.listaCartas.size == 1 || Baraja.listaCartas.isEmpty()) {
+                Baraja.crearBaraja()
+                Baraja.barajar()
+                cartaNueva = Baraja.listaCartas.last()
+            }
+            onClickDameCarta()
+        },
+        modifier = Modifier.padding(end = 5.dp),
+        colors = ButtonDefaults.buttonColors(Color.Red)
     ) {
-        Button(
-            onClick = {
-                if (Baraja.listaCartas.size == 1 || Baraja.listaCartas.isEmpty()) {
-                    Baraja.crearBaraja()
-                    Baraja.barajar()
-                }
-                onClickDameCarta()
-            },
-            modifier = Modifier.padding(end = 5.dp),
-            colors = ButtonDefaults.buttonColors(Color.Red)
-        ) {
-            Text(
-                text = "Dame carta",
-                color = Color.Black
-            )
-        }
-        Button(
-            onClick = { onClickReiniciar() },
-            colors = ButtonDefaults.buttonColors(Color.Red)
-        ) {
-            Text(
-                text = "Reiniciar",
-                color = Color.Black
-            )
-        }
+        Text(
+            text = "Dame carta",
+            color = Color.Black
+        )
+    }
+    Button(
+        onClick = { onClickReiniciar() },
+        colors = ButtonDefaults.buttonColors(Color.Red)
+    ) {
+        Text(
+            text = "Reiniciar",
+            color = Color.Black
+        )
     }
 }
+
+
+
